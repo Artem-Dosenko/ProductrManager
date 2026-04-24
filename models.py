ce8afba65e41367c599123d0f709ab072562017f
artem <artem.dos.25@gmail.com>
2026-04-24 18:52:18 +0300

    Add base patern

--- models.py
+++ models.py
@@ -6,14 +6,16 @@ class BaseModel(Model):
     class Meta:
         database = db
 
+class Company(BaseModel):
+    name = TextField()
+    password = TextField()
+
 class Product(BaseModel):
     name = TextField()
     price = FloatField()
     category = TextField()
+    company = ForeignKeyField(Company, backref='products')
 
-class Company(BaseModel):
-    name = TextField()
-    password = TextField()
 
 def init_db():
     db.connect()
